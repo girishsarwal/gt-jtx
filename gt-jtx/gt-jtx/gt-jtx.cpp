002a486d47ba08af4878a7c6301fa2d24c7ddcd4
g.sarwal@gmail.com <g.sarwal@gmail.com@79910f2a-facc-22ab-b1ea-80da55196309>
2012-09-20 09:12:14 +0000

    SYNC: attempted to add a timer
    git-svn-id: https://gt-jtx.googlecode.com/svn/trunk@3 79910f2a-facc-22ab-b1ea-80da55196309

--- gt-jtx/gt-jtx/gt-jtx.cpp
+++ gt-jtx/gt-jtx/gt-jtx.cpp
@@ -11,45 +11,59 @@
 
 #define PPMOUT 2
 
-void setup();
-
 int main(void)
-{
-	setup();
-    while(1)
-    {
-        
-    }
-};
-
-void setup()
 {
 	
 	/* set PORTA pin 1 to out **/
 	DDRB = 0b00000001;
 	
-	/** setup basic timer **/
+	/** setup outer timer 
+	*	counts per 10 ms to give a timebase for frame generation
+	*/
+	
+	TCNT0 = 0;								/** Reset the timer counter **/
+	TCCR0 |= (1<<CS02) || (1 << CS00); 		/** F_CPU/1024 **/
+	OCR0 = 156;								/** 156 * 0.00064s = 10 ms **/
+	
+	
 	TCNT1 = 0;								/** Reset the timer counter **/
 	TCCR1A |=0;
 	TCCR1B |= (1<<WGM12);					/** mode 4 - CTC **/
-	TCCR1B |= (1<<CS11) | (1<<CS10);		/** F_CPU/8**/
-	OCR1A = 2;							/** 40000 * 0.0000005s = 20 ms **/
-	TIMSK |= (1<<OCIE1A);					/** Enable Interrupt in mask **/
+	TCCR1B |= (1<<CS11);					/** F_CPU/8**/
+	OCR1A = 2000;							/** 2000 * 0.0000005s = 1 ms **/
+	
 	
 	sei();
-};	
+	
+	TIMSK |= (1<<OCIE0);					/** Enable Interrupt in mask so our 'outer' timer begins**/
 
-void ppmout()
-{
-	/** send sync **/
-	/** send pulses **/
-	PORTB = ~(PORTB);
+    while(1)
+    {
+        PORTB = 0x00;
+		/** updates to user interface etc from data values**/
+    }
 };
+
+
+
+
+ISR(TIMER0_COMP_vect)
+{
+
+	cli();
+	TIMSK &= ~(1<<OCIE0);	/** stop reentrance **/
+	TIMSK |= (1<<OCIE1A);	/** enable our 'inner' timer **/
+	TIMSK |= (1<<OCIE0);	/** enable back 'outer' timer **/
+	sei();
+}
 
 
 ISR(TIMER1_COMPA_vect)
 {	
+	PORTB = 0x01;
 	cli();
-	//TIMSK&= ~(1<<OCIE1A);
-	ppmout();		/** throw ppm signals out on pin PPMOUT **/
+	TIMSK&= ~(1<<OCIE1A);	/** stop reentrancy **/
+	/** single instruction **/
+	TIMSK |= (1<<OCIE1A);	/** enable back 'inner' timer **/
+	sei();
 }
