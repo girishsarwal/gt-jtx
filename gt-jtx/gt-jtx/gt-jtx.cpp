c71dfe4783d27b6b11556a63c76e3b60747f7f47
g.sarwal@gmail.com <g.sarwal@gmail.com@79910f2a-facc-22ab-b1ea-80da55196309>
2012-09-24 05:14:43 +0000

    successfully getting a configurable timed pulse for rc frame. This needs to be moved onto the hardware pin now
    git-svn-id: https://gt-jtx.googlecode.com/svn/trunk@5 79910f2a-facc-22ab-b1ea-80da55196309

--- gt-jtx/gt-jtx/gt-jtx.cpp
+++ gt-jtx/gt-jtx/gt-jtx.cpp
@@ -13,52 +13,74 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+
+void setup();
+void reset();
+void applyTrimTransforms();
+void applyEndPointTransforms();
+
+#define SYNC 0
+#define AIL	1
+#define ELE	2
+#define THR	3
+#define RUD	4
+#define MAX_CHANNEL RUD
+
+uint16_t ppm[(MAX_CHANNEL + 1) * 2 ];
+
+
 int main(){
-	DDRC = 0x01;
-	PORTC = 0x00;
-	TCNT1 = 0;
+	reset();
 	
-	TCCR1B |= (1<<CS10) ;				/** F_CPU - 1Mhz Timebase**/
-	TCCR1B	|=	(1<<WGM12);				/** Mode 4 **/
-	OCR1A = 22000;						/** 1/(1000000 ) * 22000= 22 ms **/
+	setup();
 	
+	/**Enable global interrupts **/
 	sei();
+
+	/** start our timer **/
 	TIMSK |= (1<<OCIE1A);
 	while(1){
-		
-		
+		/**update output display **/
+	}
+}
+uint8_t i = 1;
+static int channel = SYNC;
+ISR(TIMER1_COMPA_vect){
+	PORTC ^= (1<<0);
+	//if(channel %2 == 0){
+		//PORTC = 0x00;
+	//}
+	OCR1A = ppm[channel++];
+	if(channel > MAX_CHANNEL * 2){
+		channel = SYNC;
 	}
 }
 
-ISR(TIMER1_COMPA_vect){
-	/**Ailerons **/
-	PORTC = 0x00;
-	_delay_us(300);
-	PORTC= 0x01;
-	_delay_us(700);
-	
-	/**Elevator**/
-	PORTC = 0x00;
-	_delay_us(300);
-	PORTC= 0x01;
-	_delay_us(700);
-	
-	/** Throttle **/
-	PORTC = 0x00;
-	_delay_us(300);
-	PORTC= 0x01;
-	_delay_us(700);
-	
-	/** Rudder **/
-	PORTC = 0x00;
-	_delay_us(300);
-	PORTC= 0x01;
-	_delay_us(700);
-	
-	/**sync pulse **/
-	PORTC = 0x00;
-	_delay_us(1000);
+void setup(){
+	DDRC = 0x01;
 	PORTC = 0x01;
-
 	TCNT1 = 0;
+	
+	TCCR1B |= (1<<CS10) ;				/** F_CPU - 1Mhz Timebase - Outer Timer**/
+	TCCR1B	|=	(1<<WGM12);				/** CTC Mode**/
+	OCR1A = ppm[SYNC];						
+}
+
+void reset(){
+	/** set values to match servo spec **/
+	ppm[0] = 1000;
+	ppm[1] = 2000 - ppm[0];
+	
+	ppm[2] = 1500;
+	ppm[3] = 2000 -ppm[2];
+	
+	ppm[4] = 1500;
+	ppm[5] = 2000- ppm[4];
+	
+	ppm[6] = 500;
+	ppm[7] = 2000 - ppm[6];
+	
+	ppm[8] = 500;
+	ppm[9] = 2000 - ppm[8];
+	
 }
