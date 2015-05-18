9903f466aff0da484f53d2211383d4bc81c2043c
Girish Sarwal <g.sarwal@gmail.com>
2015-05-18 13:48:40 +0530

    - Added render queue support - removed ArtrixRenderer and implemented it in ArtrixView - Added new State SplashState

---
+++ Artrix/app/src/main/java/com/gluedtomatoes/artrix/SplashState.java
@@ -0,0 +1,23 @@
+package com.gluedtomatoes.artrix;
+
+/**
+ * Created by gsarwal on 5/18/2015.
+ */
+public class SplashState implements State{
+    public static SplashState state = new SplashState();
+    private Sprite background = new Sprite();
+    @Override
+    public void enter(StateMachine sm) {
+
+    }
+
+    @Override
+    public void update(StateMachine sm) {
+        background.update();
+    }
+
+    @Override
+    public void exit(StateMachine sm) {
+
+    }
+}
